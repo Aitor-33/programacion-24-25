0f98d63d73df51a692d448752e3cf3bd9ae2a8e3
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2024-09-30 12:44:22 +0200

    final de ejercicio 1

--- Unidad2Ejercicio1/src/com/decroly/daw/Ejercicio1.java
+++ Unidad2Ejercicio1/src/com/decroly/daw/Ejercicio1.java
@@ -74,12 +74,104 @@ public class Ejercicio1 {
 		System.out.println("son: "+(vmillas * 1.852)+ " metros");
 //EJERCICO 8		
 		
+		Scanner numeros1 = new Scanner(System.in);
+		System.out.println("introduzca un numero:");
+		double mayor = numeros1.nextDouble(); 
+		
 		
 	
-
+		Scanner numeros2 = new Scanner(System.in);
+		System.out.println("introduzca un numero:");
+		double menor = numeros2.nextDouble();
 		
+		System.out.println("los numeros en orden ascendente son:");
 		
+		double menores = Math.min(mayor, menor);
 		
+		double mayores = Math.max(mayor, menor);
+		
+		System.out.println(menores);
+		System.out.println(mayores);
+//EJERCICIO 9
+		numeros1 = new Scanner(System.in);
+		
+		System.out.println("introduce el primer numero:");
+		
+		double menor1 = numeros1.nextDouble();
+		
+		System.out.println("introduce el segundo numero:");
+		
+		double mayor1 =numeros1.nextDouble();
+		
+		System.out.println("el primer numero es mayor:"+ (menor1>mayor1));
+		
+		System.out.println("el segundo numero es mayor:"+ (menor1<mayor1));
+		
+		System.out.println("el primer numero es igual a el segundo numero:"+ (menor1==mayor1));
+//EJERCICIO 10
+		numeros1 = new Scanner(System.in);
+		
+		System.out.println("introduce el primer numero:");
+		
+		double maximo1 = numeros1.nextDouble();
+		
+		System.out.println("introduce el segundo numero:");
+		
+		double maximo2 = numeros1.nextDouble();
+		
+		System.out.println("introduce el tercer numero:");
+		
+		double maximo3 = numeros1.nextDouble();
+		
+		System.out.println("el numero mayor es:"+ (Math.max(maximo1,(Math.max(maximo2, maximo3)))));
+		
+//EJERCICIO 11
+		
+		Scanner reader4 = new Scanner(System.in);
+		
+		Scanner reader5 = new Scanner(System.in);
+	
+		System.out.println("intriduce un numero:");	
+		
+		double numero5 = reader4.nextDouble();
+		
+		System.out.println("intriduce un numero:");	
+		
+		double numero6 = reader5.nextDouble();
+		
+		System.out.println("la suma es:"+(numero5 + numero6));
+		
+		System.out.println("la resta es:"+(numero5 - numero6));
+		
+		System.out.println("el producto es:"+(numero5 * numero6));
+		
+		System.out.println("la division es:"+(numero5 / numero6));	
+//EJERCICIO 12
+		numeros1 = new Scanner(System.in);
+		
+		System.out.println("introduce el primer numero:");
+		
+		double maximo4 = numeros1.nextDouble();
+		
+		System.out.println("introduce el segundo numero:");
+		
+		double maximo5 = numeros1.nextDouble();
+		
+		System.out.println("el numero mayor es:"+(Math.max(maximo4, maximo5)));
+		
+//EJERCICIO 13		
+		
+		numeros1 = new Scanner(System.in);
+		
+		System.out.println("introduce el primer numero:");
+		
+		double maximo6 = numeros1.nextDouble();
+		
+		double valor = Math.signum(maximo6);
+		
+		System.out.println("el valor de este numero es:");
+		System.out.println(valor);
+		System.out.println("si el valor es 1 es positivo y si es -1 es negativo");
 	}
 
 }
