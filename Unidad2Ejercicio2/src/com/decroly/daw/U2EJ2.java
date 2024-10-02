56745424810c8daea3b65ea3d5379e2c1e30fd9f
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2024-10-02 08:49:56 +0200

    EMPEZAMOS EJERCICIO 2

---
+++ Unidad2Ejercicio2/src/com/decroly/daw/U2EJ2.java
@@ -0,0 +1,23 @@
+package com.decroly.daw;
+
+public class U2EJ2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//EJERCICIO 1
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
