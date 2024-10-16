ca9a24d9cc94d5ca1cfad7e85a39c6bccaba5018
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2024-10-16 09:40:09 +0200

    actividad 4 ejercicio 3 terminado

--- Unidad2Ejercicio4/src/com/decroly/daw/Actividad4.java
+++ Unidad2Ejercicio4/src/com/decroly/daw/Actividad4.java
@@ -26,29 +26,56 @@ public class Actividad4 {
 		System.out.println();
 		
 //EJERCICIO 2
-		Scanner il = new Scanner (System.in);
-		
-		int[] vectors = new int[10];
-		
-		int j;
-		
-		System.out.println("introduce los valores:");
-		
-			for(j = 0; j < vectors.length; i++)
-				vectors[j] = in.nextInt();
+		Scanner scanner = new Scanner(System.in);
+        double[] numeros = new double[10];
+        double suma = 0;
+        
 
-		System.out.println("los valores introducidos son:");
-			
-			for(j = 0; j < vectors.length; j++)
-				
-				System.out.print(vectors[j] + " ");
-			System.out.println();
+        
+        for(int j = 0; j < numeros.length; j++) {
+            System.out.print("Introduce el número " + (j + 1) + ": ");
+            	numeros[j] = scanner.nextDouble();
+        }
+
+        
+        for(int j = 0; j < numeros.length; j++) {
+            	suma += numeros[j];
+        }
+
+       
+        System.out.println("La suma de todos los números es: " + suma);
+
+     
+    
+
 		
+	//EJERCICIO 3
+        
+        Scanner reader = new Scanner(System.in);  
+        
+        int[] numeros2 = new int[10];
+        
+        double maximo = numeros2[0];
+        
+        double minimo = numeros2[0];
+        
+        for(int l = 0; l < numeros.length; l++) {
+            System.out.print("Introduce el número " + (l + 1) + ": ");
+            	numeros2[l] = (int) reader.nextDouble();
+        }
+        
+        for(int l = 1; l < numeros2.length; l++) {
+        	
+        	maximo = Math.max(maximo, numeros2[l]);
+        	minimo = Math.min(minimo, numeros2[l]);
+        	
+        }
+        
+        System.out.println("El número máximo es: " + maximo);
+        System.out.println("El número mínimo es: " + minimo);	
+	
 		
-		
-		
-		
-		
+		}	
 	}
 
-}
+
