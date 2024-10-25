52c93acaaed602a6a33db44898cef1b77e121629
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2024-10-25 09:55:44 +0200

    piedra papel o tijera empezado

---
+++ PiedraPapelTijera/src/com/daw/decroly/PiedraPapelTijera.java
@@ -0,0 +1,57 @@
+package com.daw.decroly;
+
+import java.util.Scanner;
+
+public class PiedraPapelTijera {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hola bienvenido a Piedra Papel o Tijera.");
+	System.out.println("Estas son las normas. Piedra le gana a Tijera, Tijera le gana a Papel y Papel le gana a Piedra.");
+	System.out.println("Vas a tener tres opciones las cuales tendras que seleccionar escribiendo la inicial de la opcion que deseas escojer.");	
+		
+	Scanner reader = new Scanner(System.in);	
+		
+	String eleccionJugador;
+	String eleccionMaquina;
+	
+	do {
+		System.out.println("p - piedra");
+		System.out.println("l - papel");
+		System.out.println("t - tijeras");
+		System.out.println("s - salir");
+		
+		eleccionJugador = reader.nextLine();
+		
+		 if(eleccionJugador.equals("p")){
+			 
+			 
+            }
+		 
+            else if(eleccionJugador.equals("l")){
+            	
+				 
+            	
+            
+            	}else if(eleccionJugador.equals("t")){
+            	
+            	
+            	}
+		 
+           
+		 
+            		else{
+
+            			}
+		
+	}while(!eleccionJugador.equals("s"));
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ PiedraPapelTijera/src/module-info.java
@@ -0,0 +1,2 @@
+module PiedraPapelTijera {
+}
