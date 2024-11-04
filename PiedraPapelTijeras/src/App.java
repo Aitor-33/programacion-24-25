4102d8b4f1e6c43c261b3c34b34ea4889e922d65
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2024-11-04 13:36:24 +0100

    piedra papel y tijeras terminado

--- PiedraPapelTijeras/src/App.java
+++ PiedraPapelTijeras/src/App.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
@@ -10,39 +11,84 @@ System.out.println("Hola bienvenido a Piedra Papel o Tijera.");
 	Scanner reader = new Scanner(System.in);	
 		
 	String eleccionJugador;
-	String eleccionMaquina;
+	Random random = new Random();
 	
 	do {
 		System.out.println("p - piedra");
 		System.out.println("l - papel");
 		System.out.println("t - tijeras");
 		System.out.println("s - salir");
+
+		int eleccionMaquina = random.nextInt(3);
+
+		//0 sera piedra. 1 sera papel. 2 sera tijeras
 		
 		eleccionJugador = reader.nextLine();
 		
 		 if(eleccionJugador.equals("p")){
+			
+			if(eleccionMaquina == 0){
+				
+				System.out.println("los dos habeis escogido piedra ¡¡¡empate!!!");
 			 
+			}else if(eleccionMaquina == 1){
 			 
+				System.out.println("la maquina ha escogido papel ¡¡¡has perdido!!!");
+
+			 }else if(eleccionMaquina == 2){
+
+				System.out.println("la maquina ha escogido tijeras ¡¡¡has ganado!!!");
+
+			 }
+
             }
 		 
             else if(eleccionJugador.equals("l")){
-            	
+
+            	if(eleccionMaquina == 0){
+				
+					System.out.println("la maquina ha escogido piedra ¡¡¡has ganado!!!");
+				 
+				}else if(eleccionMaquina == 1){
+				 
+					System.out.println("los dos habeis escogido papel ¡¡¡empate!!!");
+	
+				 }else if(eleccionMaquina == 2){
+	
+					System.out.println("la maquina ha escogido tijeras ¡¡¡has perdido!!!");
+	
+				 }
 				 
             	
             
             	}else if(eleccionJugador.equals("t")){
-            	
+					
+					if(eleccionMaquina == 0){
+				
+						System.out.println("la maquina ha escogido piedra ¡¡¡has perdido!!!");
+					 
+					}else if(eleccionMaquina == 1){
+					 
+						System.out.println("la maquina ha escogido papel ¡¡¡has ganado!!!");
+		
+					 }else if(eleccionMaquina == 2){
+		
+						System.out.println("los dos habeis escogido tijeras ¡¡¡empate!!!");
+		
+					 }
             	
             	}
 		 
            
 		 
             		else{
-
+						
+						
             			}
 		
 	}while(!eleccionJugador.equals("s"));
 
+	;
 
 
 
