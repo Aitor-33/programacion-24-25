4a006ac6209f62d01ff2fb422ca159430740d53a
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2024-11-13 09:42:53 +0100

    movimientos terminado y cuenta bancaria empezado

--- dawbank/dawbank/src/CuentaBancaria.java
+++ dawbank/dawbank/src/CuentaBancaria.java
@@ -1,3 +1,21 @@
 public class CuentaBancaria {
 
+private String iban;
+private String titular;
+private double saldo;
+
+private movimientos [] movimiento;
+private int nelementos;
+
+
+public CuentaBancaria(String iban, String titular, S ){
+
+}
+
+
+
+
+
+
+
 }
--- dawbank/dawbank/src/movimientos.java
+++ dawbank/dawbank/src/movimientos.java
@@ -20,7 +20,40 @@ public class movimientos {
         this.fecha = LocalDateTime.now().toString();
         this.tipo = tipo;
         this.cantidad = cantidad;
+        
+    }
 
+    public int getid() {
+        return this.id;
+    }
+    
+    public String getfecha() {
+        return this.fecha;
+    }
+    
+    public String gettipo() {
+        return this.tipo;
+    }
+    
+    public double getcantidad() {
+        return this.cantidad;
+    }
+    
+    public void setcantidad(double cantidad) {
+        if (cantidad > 0) {
+            this.cantidad = cantidad;
+            
+        }else{
+            System.out.println("el numero debe de ser mayor que 0");
+            
+        }
+    }
+    
+    public String mostrarInfoMovimientos(){
+        
+        String mostrar = String.format("movimientos - id: %s, fecha: %s, tipo: %s, cantidad: %s", this.id, this.fecha, this.tipo, this.cantidad);
+
+        return mostrar;
     }
 
 
