5666bd56ac801b3673223d0e512f01b1002317f7
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2024-11-18 12:52:18 +0100

    clase dawbank empezada

--- dawbank/dawbank/src/CuentaBancaria.java
+++ dawbank/dawbank/src/CuentaBancaria.java
@@ -17,8 +17,80 @@ this.movimiento = new movimientos[nmovimientos];
 this.nelementos = 0;
 }
 
+public String getiban(){
+    
+    return this.iban;
 
+}
 
+public String gettitular(){
+
+    return this.titular;
+
+}
+
+public double getsaldo(){
+
+    return this.saldo;
+
+}  
+
+public boolean ingreso(movimientos m1){
+
+    boolean isAdd = false;
+
+    if(m1 != null){
+
+        this.movimiento[nelementos] = m1;
+
+        this.nelementos++;
+
+        isAdd = true;
+
+    }
+    
+    return isAdd;
+    
+}
+
+public boolean eliminarLibro(movimientos m1){
+
+    boolean isRemoved = false;
+
+    if(m1 != null){
+
+        int index = -1;
+
+        for(int i = 0; i < this.movimiento.length; i++){
+
+            if(this.movimiento[i] != null){
+                
+                index = i;
+                
+            }
+        }
+        if(index != -1){
+
+            this.movimiento[index] = null;
+
+            isRemoved = true;
+            
+            for(int i = index+1; i < this.movimiento.length; i++){
+
+                this.movimiento[i-1] = this.movimiento[i];
+
+            }
+
+            this.movimiento[this.movimiento.length-1] = null;
+
+            this.nelementos--;
+
+        }
+    }
+
+    return isRemoved;
+
+}
 
 
 
--- dawbank/dawbank/src/DawBank.java
+++ dawbank/dawbank/src/DawBank.java
@@ -1,3 +1,71 @@
-public class DawBank {
+import java.util.Scanner;
 
+public class dawbank {
+    public static void main(String[] args) throws Exception {
+        
+        
+        Scanner reader = new Scanner(System.in);	
+		
+	String eleccionJugador;
+        
+        
+        
+        do {
+            System.out.println("1 - datos de la cuenta");
+            System.out.println("2 - IBAN");
+            System.out.println("3 - titular");
+            System.out.println("4 - saldo");
+            System.out.println("5 - ingreso");
+            System.out.println("6 - retirada");
+            System.out.println("7 - movimientos");
+            System.out.println("8 - salir");
+    
+            
+            eleccionJugador = reader.nextLine();
+            
+            if(eleccionJugador.equals("1")){
+                
+                
+    
+                }else if(eleccionJugador.equals("2")){
+    
+                    
+                    
+                
+                    }else if(eleccionJugador.equals("3")){
+                        
+                    
+                    
+                    }else if(eleccionJugador.equals("4")){
+
+
+
+
+                    }else if(eleccionJugador.equals("5")){
+
+
+
+
+                    }else if(eleccionJugador.equals("6")){
+
+
+
+
+                    }else if(eleccionJugador.equals("7")){
+
+
+
+        
+        
+                    }else{
+                            System.out.println("escoge una opcion valida");
+                            
+                            }
+            
+        }while(!eleccionJugador.equals("8"));
+    
+    System.out.println("has salido");  
+
+        
+    }
 }
