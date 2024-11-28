398f941bd6bd24c392b3025f8ad7c00f7ece5f7c
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2024-11-28 08:54:39 +0100

    video daw

--- videodaw/src/Pelicula.java
+++ videodaw/src/Pelicula.java
@@ -1,13 +1,34 @@
+
+import java.time.LocalDateTime;
+
 public class Pelicula {
 
     private String cod;
     private String titulo;
     private String genero;
-    private String fechaRegistro;
-    private String fechaBaja;
-    private String fechaAlquiler;
+    private LocalDateTime fechaRegistro;
+    private LocalDateTime fechaBaja;
+    private LocalDateTime fechaAlquiler;
     private boolean isAlquilada;
     
+
+    public Pelicula(String cod, String titulo, String genero, boolean isAlquilada){
+
+        this.fechaRegistro = LocalDateTime.now();
+
+        this.fechaBaja = LocalDateTime.now();
+
+        this.fechaAlquiler = LocalDateTime.now();
+
+        this.cod = cod;
+
+        this.titulo = titulo;
+
+        this.genero = genero;
+
+        this.isAlquilada = isAlquilada;
+
+    }
 
 
 
