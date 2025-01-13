86a213ca1ba53cb40f22288127522acad56c96d7
COMPU-S027\daw1 <gutierrezpuenteaitor@gmail.com>
2025-01-13 11:11:01 +0100

    herencia 2 empezado

---
+++ herencia2/Agenda1/src/Contacto.java
@@ -0,0 +1,43 @@
+public class Contacto extends Agenda{
+
+private String nombre;
+private int telefono;
+
+
+public Contacto(String nombre, int telefono) {
+
+    this.nombre = nombre;
+    this.telefono = telefono;
+    
+}
+
+public String getNombre() {
+
+    return nombre;
+
+}
+
+public int getTelefono() {
+
+    return telefono;
+
+}
+
+@Override
+public String toString() {
+
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("contacto");
+
+    sb.append("el nombre del contacto es = ").append(this.nombre);
+
+    sb.append(", su numero de telefono es = ").append(this.telefono);
+
+    return sb.toString();
+
+}
+
+
+
+}
