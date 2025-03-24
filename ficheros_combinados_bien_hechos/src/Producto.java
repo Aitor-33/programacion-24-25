ed3dddeb8f67023a4ce66812db547f8045c5fe6b
COMPU-S032\daw1 <bodean0375@gmail.com>
2025-03-24 11:34:43 +0100

    el comienzo de una nueva era

---
+++ ficheros_combinados_bien_hechos/src/Producto.java
@@ -0,0 +1,19 @@
+public class Producto {
+private String referencia;
+private String descripcion;
+private String tipo;
+private int cantidad;
+private Double precio;
+private int descuento;
+private int iva;
+
+
+
+
+
+
+
+
+
+
+}
