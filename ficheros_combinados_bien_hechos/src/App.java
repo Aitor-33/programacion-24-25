a99a6d078339f7e1c16b327d9c214bd4808370b7
COMPU-S032\daw1 <bodean0375@gmail.com>
2025-03-24 12:49:25 +0100

    ficheros combinados bien hecho solo queda eliminar

--- ficheros_combinados_bien_hechos/src/App.java
+++ ficheros_combinados_bien_hechos/src/App.java
@@ -1,5 +1,53 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        List <Producto> productos = new LinkedList<>();
+
+    //leer el fichero csv
+    try (FileReader file = new FileReader("Recursos\\productos.csv"); BufferedReader reader = new BufferedReader(file);) {
+            String linea = reader.readLine();
+            while(linea != null){
+                
+                String[] datos = linea.split("/");
+                
+                String referencia = datos[0];
+                String nombre = datos[1];
+                String descripcion = datos[2];
+                String tipo = datos[3];
+
+                Producto p =  new Producto(referencia, nombre, descripcion, tipo);
+
+            
+                productos.add(p);
+
+                linea = reader.readLine();
+            }
+
+            
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Pinto por pantalla mis lista de productos
+        for (Producto linea : productos) {
+            System.out.println(linea);
+        }
+
+        //eliminar productos
+
+
+
+
+
+
+
+
     }
 }
--- ficheros_combinados_bien_hechos/src/Producto.java
+++ ficheros_combinados_bien_hechos/src/Producto.java
@@ -1,11 +1,104 @@
 public class Producto {
 private String referencia;
+private String nombre;
 private String descripcion;
 private String tipo;
 private int cantidad;
 private Double precio;
 private int descuento;
 private int iva;
+
+
+public Producto(String referencia, String nombre, String descripcion, String tipo) {
+
+    this.referencia = referencia;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.tipo = tipo;
+
+}
+public Producto(String referencia, String nombre, String descripcion, String tipo, int cantidad, Double precio,
+        int descuento, int iva) {
+
+    this.referencia = referencia;
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.tipo = tipo;
+    this.cantidad = cantidad;
+    this.precio = precio;
+    this.descuento = descuento;
+    this.iva = iva;
+    
+}
+public String getReferencia() {
+    return referencia;
+}
+public String getNombre() {
+    return nombre;
+}
+public String getDescripcion() {
+    return descripcion;
+}
+public String getTipo() {
+    return tipo;
+}
+public int getCantidad() {
+    return cantidad;
+}
+public Double getPrecio() {
+    return precio;
+}
+public int getDescuento() {
+    return descuento;
+}
+public int getIva() {
+    return iva;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+}
+public void setTipo(String tipo) {
+    this.tipo = tipo;
+}
+public void setCantidad(int cantidad) {
+    this.cantidad = cantidad;
+}
+public void setPrecio(Double precio) {
+    this.precio = precio;
+}
+public void setDescuento(int descuento) {
+    this.descuento = descuento;
+}
+public void setIva(int iva) {
+    this.iva = iva;
+}
+
+    @Override
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Producto{");
+        sb.append("referencia=").append(referencia);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", descripcion=").append(descripcion);
+        sb.append(", tipo=").append(tipo);
+        sb.append(", cantidad=").append(cantidad);
+        sb.append(", precio=").append(precio);
+        sb.append(", descuento=").append(descuento);
+        sb.append(", iva=").append(iva);
+        sb.append('}');
+
+        return sb.toString();
+        
+    }
+
+
+
+
 
 
 
