7603ce93a53e1205503038f8c86d8a43a584b1f9
COMPU-S032\daw1 <p3976005@gmail.com>
2025-05-13 08:10:35 +0200

    aaa

--- clases-parking/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.clasesparking {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens org.example.clasesparking to javafx.fxml;
-    exports org.example.clasesparking;
-}
