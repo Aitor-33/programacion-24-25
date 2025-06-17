d2e385a50756c48c653b8300d8e3f5f1e57116b9
COMPU-S032\daw1 <p3976005@gmail.com>
2025-06-17 12:42:48 +0200

    clase de repaso final

--- Repaso_Examen/src/main/java/org/example/repaso_examen/VetAppController.java
+++ Repaso_Examen/src/main/java/org/example/repaso_examen/VetAppController.java
@@ -1,19 +1,39 @@
 package org.example.repaso_examen;
 
 import javafx.beans.Observable;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class VetAppController {
+public class VetAppController implements Initializable {
     @FXML
     private Label welcomeText;
+
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private ComboBox<Propietario> comboBoxPrueba;
+
+    @FXML
+    protected void ComboBox onComboPrueba(ActionEvent Action){
+
+    }
+    
 
     //objeto de sql
     private SQLAccessVetDaw sql = new SQLAccessVetDaw();
@@ -23,6 +43,24 @@ public class VetAppController {
     //dentro de inizializable
     List<Mascota> mascotas = sql.getAllMascotas();
 
-    (aqui va la observablelist).addAll(mascotas);
-    listview.setItems(mascotas);
+//    (aqui va la observablelist).addAll(mascotas);
+//    listview.setItems(mascotas);
+
+
+
+    //utilizacion del epoch
+    //dentro del inizialice
+
+
+Mascota mascota;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ZoneId zoneId = ZoneId.of("Europe/Madrid");
+        LocalDateTime.now().atZone(zoneId).getOffset();
+
+        long fechaLong = LocalDateTime.now().toInstant(LocalDateTime.now().atZone(zoneId).getOffset()).getEpochSecond();
+
+        Consulta con = new Consulta(fechaLong, 60, "nada", mascota);
+    }
 }
